e8acc65ee7f1878ae3237be997264e617fc772fb
josebuen099 <132584288+josebuen099@users.noreply.github.com>
2024-02-23 08:45:49 -0500

    se cambia el estado del contador

--- prueba/aplicacion_2/lib/presentacion/screens/counter_screen.dart
+++ prueba/aplicacion_2/lib/presentacion/screens/counter_screen.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
 
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('contador material 3'),
       ),
-      body: const Center(
+      body:  Center(
         
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('10',
-                style: TextStyle(fontSize: 160, fontWeight: FontWeight.bold)),
-            Text('clicks', style: TextStyle(fontSize: 25))
+            Text('$clickCounter',
+                style: const TextStyle(fontSize: 160, fontWeight: FontWeight.bold)),
+            
+            const Text('clicks', style: TextStyle(fontSize: 25))
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          
+          setState(() {
+            clickCounter++;
+          });
+        },
         child: const Icon(Icons.plus_one),
       ),
     );
