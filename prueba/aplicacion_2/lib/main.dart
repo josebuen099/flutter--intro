de100e214a95def3a4764cab481fac55bd59de9d
josebuen099 <132584288+josebuen099@users.noreply.github.com>
2024-02-23 06:39:12 -0500

    Se crea nueva ventana

--- prueba/aplicacion_2/lib/main.dart
+++ prueba/aplicacion_2/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:aplicacion_2/presentacion/screens/counter_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,8 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          body: Center(child: Text('hola mundo')),
-        ));
+        home:CounterScreen());
   }
 }
